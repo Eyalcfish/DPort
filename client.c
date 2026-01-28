40128577df9bb78e328b3688eef93ed233268a45
fishministrator <yulucohen@gmail.com>
2026-01-28 14:35:52 +0200

    changed spaces

--- client.c
+++ client.c
@@ -3,12 +3,16 @@
 
 int main(int argc, char** argv) {
     DConnection* conn = connect_dconnection("example_port");
+
     printf("DEBUG: Flag address: %p | Data address: %p\n", 
        (void*)((char*)conn->shm_ptr - 1), conn->shm_ptr);
+
     DMessage msg;
     msg.size = strlen(argv[1]) + 1;
     msg.data = argv[1];
+
     write_to_dconnection(conn, &msg);
+
     close_dconnection(conn);
     return 0;
 }
--- dport.c
+++ dport.c
@@ -17,9 +17,12 @@
 /*Create a shared memory connection at id/port <port> and size <shm_size> */
 DConnection* create_dconnection(const char* port_name, size_t shm_size) {
     DConnection* conn = (DConnection*)malloc(sizeof(DConnection));
+
     conn->port_name = strdup(port_name);
     conn->shm_size = shm_size;
+
     shm_size += sizeof(DConnectionHeader);
+
     #ifdef _WIN32
     
     HANDLE hMapFile = CreateFileMapping(
@@ -35,15 +38,10 @@ DConnection* create_dconnection(const char* port_name, size_t shm_size) {
         printf(TEXT("Could not create a connnection(Prob outta memory)\n"), GetLastError());
     }
     
-conn->shm_ptr = (void*) MapViewOfFile(hMapFile,
-    FILE_MAP_ALL_ACCESS,
-    0,
-    0,
-    shm_size);
+    conn->shm_ptr = (void*) MapViewOfFile(hMapFile, FILE_MAP_ALL_ACCESS, 0, 0, shm_size);
     
     if (conn->shm_ptr == NULL) {
-        printf(TEXT("Could not map view of file (%d).\n"),
-        GetLastError());
+        printf(TEXT("Could not map view of file (%d).\n"), GetLastError());
         
         CloseHandle(hMapFile);
     }
@@ -55,6 +53,7 @@ conn->shm_ptr = (void*) MapViewOfFile(hMapFile,
 
     conn->hEvent = CreateEvent(NULL, FALSE, FALSE, event_name);
     #endif
+
     *((DConnectionHeader*)conn->shm_ptr) = (DConnectionHeader){.shm_size = conn->shm_size};
     conn->shm_ptr += sizeof(DConnectionHeader);
 
@@ -65,8 +64,8 @@ conn->shm_ptr = (void*) MapViewOfFile(hMapFile,
 DConnection* connect_dconnection(const char* port_name) {
     DConnection* conn = (DConnection*)malloc(sizeof(DConnection));
     conn->port_name = strdup(port_name);
-    #ifdef _WIN32
 
+    #ifdef _WIN32
     HANDLE hMapFile = OpenFileMapping(
         FILE_MAP_ALL_ACCESS,
         FALSE,
@@ -77,26 +76,25 @@ DConnection* connect_dconnection(const char* port_name) {
         printf(TEXT("Could not open file mapping object (%d).\n"), GetLastError());
     }
 
-    conn->shm_ptr = (void*) MapViewOfFile(hMapFile,
-        FILE_MAP_ALL_ACCESS,
-        0,
-        0,
-        0);
+    conn->shm_ptr = (void*) MapViewOfFile(hMapFile, FILE_MAP_ALL_ACCESS, 0, 0, 0);
 
     if (conn->shm_ptr == NULL) {
-      printf(TEXT("Could not map view of file (%d).\n"), GetLastError());
-
-       CloseHandle(hMapFile);
+        printf(TEXT("Could not map view of file (%d).\n"), GetLastError());
+        CloseHandle(hMapFile);
     }
+
     conn->hMapFile = hMapFile;
 
     char event_name[256];
     snprintf(event_name, sizeof(event_name), "%s_event", port_name);
+
     conn->hEvent = OpenEvent(EVENT_ALL_ACCESS, FALSE, event_name);
     
     #endif
+
     conn->shm_size = ((DConnectionHeader*)conn->shm_ptr)->shm_size;
     conn->shm_ptr += sizeof(DConnectionHeader);
+
     return conn;
 }
 
@@ -114,9 +112,12 @@ void write_to_dconnection(DConnection* conn, DMessage* msg) {
         printf("Message size exceeds shared memory size\n");
         return;
     }
+
     *((size_t*)conn->shm_ptr) = msg->size;
     memcpy((char*)conn->shm_ptr+sizeof(size_t), msg->data, msg->size);
+    #ifdef _WIN32
     SetEvent(conn->hEvent);
+    #endif
 }
 
 /*Read data from the shared memory connection */
@@ -125,8 +126,10 @@ DMessage wait_for_new_message_from_dconnection(DConnection* conn) {
     #ifdef _WIN32
     WaitForSingleObject(conn->hEvent, INFINITE);
     #endif
+
     DMessage msg;
     msg.size = * (size_t*)conn->shm_ptr;
     msg.data = conn->shm_ptr+sizeof(size_t);
+
     return msg;
 }
