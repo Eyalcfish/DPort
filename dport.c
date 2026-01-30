7233fce6507e5d78c2be51865368e6d13f278ef6
fishministrator <yulucohen@gmail.com>
2026-01-30 18:57:52 +0200

    added python lib support

--- dport.c
+++ dport.c
@@ -1,13 +1,18 @@
 #include "dport.h"
+#include <stdlib.h> // for malloc, free
+
 #ifdef _WIN32
+    // windows.h and intrin.h are already included via dport.h
 #elif __linux__
-#include <immintrin.h>    // for _mm_pause()
-#include <unistd.h>      // for syscall, close, ftruncate
-#include <sys/syscall.h> // for SYS_futex
-#include <linux/futex.h> // for FUTEX_WAIT, FUTEX_WAKE
-#include <errno.h>       // for errno, EWOULDBLOCK
-#include <string.h>      // for memcpy, strdup
-#include <stdint.h>      // for int32_t
+    #include <immintrin.h>   // for _mm_pause()
+    #include <unistd.h>      // for syscall, close, ftruncate
+    #include <sys/syscall.h> // for SYS_futex
+    #include <linux/futex.h> // for FUTEX_WAIT, FUTEX_WAKE
+    #include <errno.h>       // for errno, EWOULDBLOCK
+    #include <string.h>      // for memcpy, strdup
+    #include <stdint.h>      // for int32_t
+    #include <sys/mman.h>    // for mmap, shm_open
+    #include <fcntl.h>       // for O_CREAT, O_RDWR
 #endif
 
 typedef struct __attribute__((packed)) DConnectionHeader {
@@ -17,12 +22,13 @@ typedef struct __attribute__((packed)) DConnectionHeader {
     unsigned char ready_flag_client;
     #ifdef _WIN32
     #elif __linux__
-    int futex_flag
+    int futex_flag;
     #endif
 } DConnectionHeader;
 
-/*Create a shared memory connection at id/port <port> and size <shm_size> */
-DConnection* create_dconnection(const char* port_name, size_t shm_size, char connection_type) {
+// --- IMPLEMENTATIONS ---
+
+DPORT_EXPORT DConnection* create_dconnection(const char* port_name, size_t shm_size, char connection_type) {
     DConnection* conn = (DConnection*)malloc(sizeof(DConnection));
 
     conn->port_name = strdup(port_name);
@@ -31,8 +37,7 @@ DConnection* create_dconnection(const char* port_name, size_t shm_size, char con
     shm_size += sizeof(DConnectionHeader);
 
     #ifdef _WIN32
-    
-    HANDLE hMapFile = CreateFileMapping(
+    HANDLE hMapFile = CreateFileMappingA(
         INVALID_HANDLE_VALUE,
         NULL,
         PAGE_READWRITE,
@@ -42,14 +47,13 @@ DConnection* create_dconnection(const char* port_name, size_t shm_size, char con
     );
     
     if (hMapFile == NULL) {
-        printf(TEXT("Could not create a connnection(Prob outta memory)\n"), GetLastError());
+        printf("Could not create a connnection (Prob outta memory): %lu\n", GetLastError());
     }
     
-    conn->shm_ptr = (void*) MapViewOfFile(hMapFile, FILE_MAP_ALL_ACCESS, 0, 0, shm_size);
+    conn->shm_ptr = (void*) MapViewOfFile(hMapFile, FILE_MAP_ALL_ACCESS, 0, 0, 0); // Map entire file
     
     if (conn->shm_ptr == NULL) {
-        printf(TEXT("Could not map view of file (%d).\n"), GetLastError());
-        
+        printf("Could not map view of file (%lu).\n", GetLastError());
         CloseHandle(hMapFile);
     }
 
@@ -58,13 +62,11 @@ DConnection* create_dconnection(const char* port_name, size_t shm_size, char con
     char event_name[256];
     snprintf(event_name, sizeof(event_name), "%s_event", port_name);
 
-    conn->hEvent = CreateEvent(NULL, FALSE, FALSE, event_name);
+    conn->hEvent = CreateEventA(NULL, FALSE, FALSE, event_name);
     #elif __linux__
 
     int shm_fd = shm_open(port_name, O_CREAT | O_RDWR, 0666);
-
     ftruncate(shm_fd, shm_size);
-
     conn->shm_ptr = mmap(0, shm_size, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
 
     #endif
@@ -75,33 +77,31 @@ DConnection* create_dconnection(const char* port_name, size_t shm_size, char con
     #else
     *((DConnectionHeader*)conn->shm_ptr) = (DConnectionHeader){.shm_size = conn->shm_size, .connection_type = conn->connection_type, .ready_flag_client = 1, .ready_flag_server = 1};
     #endif
-    conn->shm_ptr += sizeof(DConnectionHeader);
+    conn->shm_ptr = (char*)conn->shm_ptr + sizeof(DConnectionHeader);
 
     conn->identifier = 1;
-
     return conn;
 }
 
-/*Connect to an existing shared memory connection at id/port <port> */
-DConnection* connect_dconnection(const char* port_name) {
+DPORT_EXPORT DConnection* connect_dconnection(const char* port_name) {
     DConnection* conn = (DConnection*)malloc(sizeof(DConnection));
     conn->port_name = strdup(port_name);
 
     #ifdef _WIN32
-    HANDLE hMapFile = OpenFileMapping(
+    HANDLE hMapFile = OpenFileMappingA(
         FILE_MAP_ALL_ACCESS,
         FALSE,
         port_name
     );
 
     if (hMapFile == NULL) {
-        printf(TEXT("Could not open file mapping object (%d).\n"), GetLastError());
+        printf("Could not open file mapping object (%lu).\n", GetLastError());
     }
 
     conn->shm_ptr = (void*) MapViewOfFile(hMapFile, FILE_MAP_ALL_ACCESS, 0, 0, 0);
     
     if (conn->shm_ptr == NULL) {
-        printf(TEXT("Could not map view of file (%d).\n"), GetLastError());
+        printf("Could not map view of file (%lu).\n", GetLastError());
         CloseHandle(hMapFile);
     }
     
@@ -110,19 +110,16 @@ DConnection* connect_dconnection(const char* port_name) {
     char event_name[256];
     snprintf(event_name, sizeof(event_name), "%s_event", port_name);
     
-    conn->hEvent = OpenEvent(EVENT_ALL_ACCESS, FALSE, event_name);
-    
+    conn->hEvent = OpenEventA(EVENT_ALL_ACCESS, FALSE, event_name);
 
     #elif __linux__
 
     int shm_fd = shm_open(port_name, O_RDWR, 0666);
-
+    // Map header first to read size
     conn->shm_ptr = ((DConnectionHeader*)mmap(0, sizeof(DConnectionHeader), PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0));
-
     size_t shm_size = ((DConnectionHeader*)conn->shm_ptr)->shm_size + sizeof(DConnectionHeader);
-
     munmap((void*)((DConnectionHeader*)conn->shm_ptr), sizeof(DConnectionHeader));
-
+    // Remap full size
     conn->shm_ptr = mmap(0, shm_size, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
     conn->futex_flag = &((DConnectionHeader*)conn->shm_ptr)->futex_flag;
 
@@ -130,28 +127,28 @@ DConnection* connect_dconnection(const char* port_name) {
     
     conn->shm_size = ((DConnectionHeader*)conn->shm_ptr)->shm_size;
     conn->connection_type = ((DConnectionHeader*)conn->shm_ptr)->connection_type;
-    conn->shm_ptr += sizeof(DConnectionHeader);
+    conn->shm_ptr = (char*)conn->shm_ptr + sizeof(DConnectionHeader);
 
     conn->identifier = 0;
-
     return conn;
 }
 
-/*Close the shared memory connection and free resources */
-void close_dconnection(DConnection* conn) {
+DPORT_EXPORT void close_dconnection(DConnection* conn) {
     #ifdef _WIN32
     UnmapViewOfFile((char*)conn->shm_ptr - sizeof(DConnectionHeader));
     CloseHandle(conn->hMapFile);
+    CloseHandle(conn->hEvent); // Don't forget to close the event handle
     #elif __linux__
     munmap((void*)((char*)conn->shm_ptr - sizeof(DConnectionHeader)), conn->shm_size + sizeof(DConnectionHeader));
     if (conn->identifier == 1) {
         shm_unlink(conn->port_name);
     }
     #endif
+    free(conn->port_name);
+    free(conn);
 }
 
-/*Write data to the shared memory connection */
-void write_to_dconnection(DConnection* conn, DMessage* msg) {
+DPORT_EXPORT void write_to_dconnection(DConnection* conn, DMessage* msg) {
     if (msg->size > conn->shm_size) {
         printf("Message size exceeds shared memory size\n");
         return;
@@ -160,8 +157,7 @@ void write_to_dconnection(DConnection* conn, DMessage* msg) {
     DConnectionHeader* conn_header = ((DConnectionHeader*)((char*)conn->shm_ptr - sizeof(DConnectionHeader)));
     unsigned char* flag_ptr = (conn->identifier == 0) ? &conn_header->ready_flag_server : &conn_header->ready_flag_client;
     
-    
-    while (!*flag_ptr) { // FOR NOW UNTILL SEPERATE THREAD QUEING IS IMPLEMENTED
+    while (!*flag_ptr) { 
         _mm_pause();
     }
 
@@ -181,11 +177,10 @@ void write_to_dconnection(DConnection* conn, DMessage* msg) {
         syscall(SYS_futex, conn->futex_flag, FUTEX_WAKE, 1, NULL, NULL, 0);
         #endif
     }
-    
 }
 
-/*Read data from the shared memory connection */
-DMessage wait_for_new_message_from_dconnection(DConnection* conn) {
+// --- ORIGINAL FUNCTION (Returns Struct - Safe for C, Crash for Python) ---
+DPORT_EXPORT DMessage wait_for_new_message_from_dconnection(DConnection* conn) {
     
     DConnectionHeader* conn_header = ((DConnectionHeader*)((char*)conn->shm_ptr - sizeof(DConnectionHeader)));
     unsigned char* flag_ptr = (conn->identifier == 0) ? &conn_header->ready_flag_client : &conn_header->ready_flag_server;
@@ -202,14 +197,12 @@ DMessage wait_for_new_message_from_dconnection(DConnection* conn) {
         }
         #ifdef _WIN32
         while (*flag_ptr) {
-            // Use a timeout (e.g. 10ms) to "self-heal" if a signal is missed
             WaitForSingleObject(conn->hEvent, 1);
         }
         #elif __linux__
         int expected = *conn->futex_flag;
         while (*flag_ptr) {
             int ret = syscall(SYS_futex, conn->futex_flag, FUTEX_WAIT, expected, NULL, NULL, 0);
-        
             if (ret == -1 && errno == EWOULDBLOCK) {
                 expected = *conn->futex_flag;
             }
@@ -227,4 +220,17 @@ DMessage wait_for_new_message_from_dconnection(DConnection* conn) {
     __sync_lock_test_and_set(flag_ptr, 1);
  
     return msg;
+}
+
+// --- NEW HELPER: Safe Wrapper for Python ---
+DPORT_EXPORT void wait_for_new_message_safe(DConnection* conn, DMessage* out_msg) {
+    // Call the original function
+    DMessage temp = wait_for_new_message_from_dconnection(conn);
+    // Copy result to the pointer provided by Python
+    *out_msg = temp;
+}
+
+// --- NEW HELPER: Memory Cleanup ---
+DPORT_EXPORT void free_dport_memory(void* ptr) {
+    if (ptr) free(ptr);
 }
--- dport.h
+++ dport.h
@@ -1,9 +1,20 @@
 #ifndef DPORT_H
 #define DPORT_H
+
 #include <stdio.h>
 
+// --- 1. EXPORT MACROS & WINDOWS HEADERS ---
 #ifdef _WIN32
-#include <windows.h>
+    #include <windows.h>
+    #include <intrin.h> // For _mm_pause on MSVC
+    #define DPORT_EXPORT __declspec(dllexport)
+#else
+    #define DPORT_EXPORT
+#endif
+
+// --- 2. PREVENT NAME MANGLING ---
+#ifdef __cplusplus
+extern "C" {
 #endif
 
 #define EVENT_BASED_CONNECTION 0
@@ -17,8 +28,8 @@ typedef struct DConnection {
     void* shm_ptr;
     char connection_type;
     #ifdef _WIN32
-    HANDLE hMapFile;
-    HANDLE hEvent;
+    void* hMapFile; // Using void* is safer for generic headers, but HANDLE works if windows.h is included
+    void* hEvent;
     #elif __linux__
     int* futex_flag;
     #endif
@@ -29,19 +40,33 @@ typedef struct DMessage {
     void* data;
 } DMessage;
 
-/*Create a shared memory connection at id/port <port> and size <shm_size> at most */
-DConnection* create_dconnection(const char* port_name, size_t shm_size, char connection_type);
+// --- 3. EXPORTED FUNCTIONS ---
 
-/*Connect to an existing shared memory connection at id/port <port> */
-DConnection* connect_dconnection(const char* port_name);
+/* Create a shared memory connection */
+DPORT_EXPORT DConnection* create_dconnection(const char* port_name, size_t shm_size, char connection_type);
 
-/*Close the shared memory connection and free resources */
-void close_dconnection(DConnection* conn);
+/* Connect to an existing shared memory connection */
+DPORT_EXPORT DConnection* connect_dconnection(const char* port_name);
 
-/*Write data to the shared memory connection */
-void write_to_dconnection(DConnection* conn, DMessage* msg);
+/* Close the shared memory connection and free resources */
+DPORT_EXPORT void close_dconnection(DConnection* conn);
 
-/*Read data from the shared memory connection */
-DMessage wait_for_new_message_from_dconnection(DConnection* conn);
+/* Write data to the shared memory connection */
+DPORT_EXPORT void write_to_dconnection(DConnection* conn, DMessage* msg);
+
+/* Read data (Original C Version - Returns Struct by Value) */
+DPORT_EXPORT DMessage wait_for_new_message_from_dconnection(DConnection* conn);
+
+// --- 4. PYTHON/CFFI HELPERS ---
+
+/* Safe wrapper for Python: Takes a pointer instead of returning a struct by value */
+DPORT_EXPORT void wait_for_new_message_safe(DConnection* conn, DMessage* out_msg);
+
+/* Helper to free memory allocated by this DLL */
+DPORT_EXPORT void free_dport_memory(void* ptr);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
